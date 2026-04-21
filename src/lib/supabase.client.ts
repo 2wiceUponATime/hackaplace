b4808e968ae39c39422ffb37f4eddd2709e1c446
2wiceUponATime <2wiceuponatime@proton.me>
2026-04-20 20:52:50 -0400

    Use filtered server env for client side

--- src/lib/supabase.client.ts
+++ src/lib/supabase.client.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./supabase.types";
+import { ClientEnv } from "./utils";
 
-export function createBrowserClient() {
-    return createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
+export function createBrowserClient(env: ClientEnv) {
+    return createClient<Database>(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_KEY);
 }
